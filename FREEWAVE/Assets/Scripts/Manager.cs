31b97c749a0dc4f3c1b1c175590eaecd93a33505
jessenatter <jessenatter@gmail.com>
2025-11-09 20:26:42 -0500

    file setup

---
+++ FREEWAVE/Assets/Scripts/Manager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Manager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
