5aefe9f505ff7c1ea8e50da04f255213c622cffa
jessenatter <jessenatter@gmail.com>
2025-11-20 21:01:19 -0500

    eeee

--- FREEWAVE/Assets/Scripts/LimbManager.cs
+++ FREEWAVE/Assets/Scripts/LimbManager.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 
 public class LimbManager : MonoBehaviour
 {
-    Vector2 initTargetPos;
+    [SerializeField] GameObject orgin;
+    [SerializeField] Character character;
+    Vector2 initOffsetFromOrgin;
     int currentPoint = 0;
     public limbState currentLimbState;
     float currentTime;
@@ -12,12 +14,17 @@ public class LimbManager : MonoBehaviour
         public List<Vector2> points = new List<Vector2>();
         public float duration;
         public bool loop;
-        public limbState(List<Vector2> _points,float _duration,bool _loop) { }
+        public limbState(List<Vector2> _points,float _duration,bool _loop) 
+        { 
+            points = _points; 
+            duration = _duration;
+            loop = _loop;
+        }
     }
 
     void Start()
     {
-        initTargetPos = transform.position;
+        initOffsetFromOrgin = transform.position - orgin.transform.position;
     }
 
     void Update()
@@ -26,14 +33,7 @@ public class LimbManager : MonoBehaviour
     }
     void FixedUpdate()
     {
-        currentTime++;
-        if(currentTime == currentLimbState.duration)
-        {
-            if(currentLimbState.loop)
-                Loop();
-            else 
-                ExitState();
-        }
+
     }
     void UpdatePoints()
     {
@@ -41,15 +41,35 @@ public class LimbManager : MonoBehaviour
         Vector2 nextPoint = currentLimbState.points[currentPoint + 1];
         
         float durationPerPoint = currentLimbState.duration / (currentLimbState.points.Count - 1);
+
+        if (currentTime >= (currentPoint + 1) * durationPerPoint)
+        {
+            currentPoint++;
+
+            if (currentPoint >= currentLimbState.points.Count - 1)
+            {
+                if(currentLimbState.loop)
+                    Loop();
+                else 
+                    ExitState();
+            }
+
+            thisPoint = currentLimbState.points[currentPoint];
+            nextPoint = currentLimbState.points[currentPoint + 1];
+        }
+
         float lerpAmmount = (currentTime % durationPerPoint) / durationPerPoint;
 
-        Vector2 exactTarget = Vector2.Lerp(thisPoint, nextPoint,lerpAmmount);
-        float _speed = 1f;
+        Vector2 characterDirectionVec = new Vector2(Mathf.Sign(character.transform.localScale.x),1);
+        Vector2 initRestPos = (Vector2)orgin.transform.position + (initOffsetFromOrgin * characterDirectionVec);
+        Vector2 exactTarget = initRestPos + Vector2.Lerp(thisPoint, nextPoint,lerpAmmount);
+        float _speed = 2f;
         transform.position = Vector2.Lerp(transform.position,exactTarget,Time.deltaTime * _speed);
     }
     void Loop()
     {
         currentTime = 0;
+        currentPoint = 0;
     }
     public void ExitState()
     {
