ee0ceddf9da30e5eb8f748bc50c88b3b1fa34dda
jessenatter <125529115+jessenatter@users.noreply.github.com>
2025-12-03 19:49:24 -0500

    bbb

--- FREEWAVE/Assets/Scripts/characters/ZombieAnimator.cs
+++ FREEWAVE/Assets/Scripts/characters/ZombieAnimator.cs
@@ -19,6 +19,7 @@ public class ZombieAnimator : CharacterAnimator
         upperBodyRun.rotationDuration = 20f;
         upperBodyRun.duration = 50f;
 
+        /*
         //charge attack
         LimbManager.limbState _hurt = new LimbManager.limbState(chargeAttack.transform.GetChild(0).gameObject,0.1f,false,frontLeg,chargeAttackTime);
         lowerBodyHurt = new lowerBodyState(_hurt,_hurt,this,1);
@@ -26,5 +27,7 @@ public class ZombieAnimator : CharacterAnimator
         Vector2 upperBodySpine2HurtRotation = new Vector2(30,30);
         LimbManager.limbState _upperHurt = new LimbManager.limbState(chargeAttack.transform.GetChild(1).gameObject,0.1f,false,frontArm,chargeAttackTime);
         upperBodyHurt = new upperBodyState(_upperHurt,_upperHurt,this,Vector2.zero,upperBodySpine2HurtRotation,Vector2.zero,1,false,hurtStateDuration);
+
+        */
     }
 }
