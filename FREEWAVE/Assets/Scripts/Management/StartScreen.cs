58c3cf98deaf8a4c3d4f3e4804de64759bc9c9a1
jessenatter <jessenatter@gmail.com>
2026-07-14 15:30:27 -0400

    startScreen

---
+++ FREEWAVE/Assets/Scripts/Management/StartScreen.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartScreen : MonoBehaviour
+{
+    public void NewGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Continue()
+    {
+        
+    }
+}
