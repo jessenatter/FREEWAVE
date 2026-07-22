98cdb36acbf0bbca206e721d0c5b70f0bddaf6a0
jessenatter <jessenatter@gmail.com>
2026-07-22 15:34:33 -0400

    prefabStuff

--- FREEWAVE/Assets/Scripts/Environment/BreakableWall.cs
+++ FREEWAVE/Assets/Scripts/Environment/BreakableWall.cs
@@ -4,7 +4,7 @@ public class BreakableWall : Breakable
 {
     override public void Break()
     {
-        base.Break();
         brokenObject.GetComponent<Wall>().wallColor = GetComponent<Wall>().wallColor;
+        base.Break();
     }
 }
