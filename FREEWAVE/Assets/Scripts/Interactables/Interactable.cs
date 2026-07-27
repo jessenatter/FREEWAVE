d216c685cae18d8a999fcd063a3458a8e28e4a30
jessenatter <jessenatter@gmail.com>
2026-07-27 13:28:22 -0400

    eee

--- FREEWAVE/Assets/Scripts/Interactables/Interactable.cs
+++ FREEWAVE/Assets/Scripts/Interactables/Interactable.cs
@@ -6,10 +6,23 @@ public class Interactable : MonoBehaviour
 
     [HideInInspector]public bool canInteract = true;
 
+    int interactPriority = 0;
+
     void Start()
     {
         Manager.Instance.interactables.Add(this);
         interactPrompt = transform.GetChild(0).gameObject;
+
+        Vector2 initPos = interactPrompt.transform.position;
+
+        interactPrompt.transform.SetParent(null);
+        float size = 3f;
+        interactPrompt.transform.localScale = new Vector3(size,size,size);
+        interactPrompt.transform.SetParent(this.transform);
+        interactPrompt.transform.position = initPos;
+        interactPrompt.transform.SetAsFirstSibling();
+
+        interactPrompt.SetActive(false);
     }
     
     public virtual void Interact()
